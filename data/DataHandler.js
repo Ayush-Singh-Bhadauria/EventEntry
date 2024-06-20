e56cb5daf323ebb3fed89ca144484e7412366e16
Ayush Singh Bhadauria <23f1001793@ds.study.iitm.ac.in>
2024-06-21 00:59:38 +0530

    data handlers

---
+++ data/DataHandler.js
@@ -0,0 +1,8 @@
+export default class DataHandler {
+    async load() {
+        throw new Error('load method not implemented');
+    }
+    async update() {
+        throw new Error('update method not implemented');
+    }
+}
