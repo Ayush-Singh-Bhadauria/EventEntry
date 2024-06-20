6b6b2f777c7c53cae90c95afa3d33dd377f8148f
Ayush Singh Bhadauria <23f1001793@ds.study.iitm.ac.in>
2024-06-21 00:58:56 +0530

    Navigation implemented, csv file seperated from main, bug fixes

--- app/index.js
+++ app/index.js
@@ -3,9 +3,31 @@ import { Link } from 'expo-router';
 
 export default function Page() {
     return (
-    <View>
-        <Link href='/qrgen' style={{height: 30}}>Generate QR</Link>
-        <Link href='/scanner' style={{height: 30}}>Scan Entry</Link>
+    <View style={styles.container}>
+        <Link href='/qrgen' style={styles.link}>Generate QR</Link>
+        <Link href='/scanner' style={styles.link}>Scan Entry</Link>
     </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+    },
+    link: {
+        height: 50,
+        width: '100%',
+        marginBottom: 20,
+        backgroundColor: '#007AFF',
+        padding: 10,
+        borderRadius: 8,
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textDecorationLine: 'none',
+    },
+});
--- app/scanner.js
+++ app/scanner.js
@@ -7,76 +7,33 @@ import { useState, useRef, createContext, useContext, useEffect } from 'react';
 import ScannedItem from './../components/scannedItem';
 import { AntDesign } from '@expo/vector-icons';
 import * as Sharing from 'expo-sharing';
-import JWT, { SupportedAlgorithms } from 'expo-jwt';
 import styles from './../styles/styles';
+import CSVDataHandler from '../data/csvDataHandler';
 
 const AppContext = createContext();
+const attendeesFileUri = FileSystem.documentDirectory + 'Attendence.csv';
+const dataHandler = new CSVDataHandler(attendeesFileUri);
 
 export default function App() {
-  const [permission, requestPermission] = useCameraPermissions();
   const [scannedList, setScannedList] = useState([]);
   const [secretKey, setSecretKey] = useState('TechnicalTeam')
   const [isInfoVisible, setInfoVisible] = useState(false);
-  const [showOverlay, setShowOverlay] = useState(false)
   const [overlayColor, setOverlayColor] = useState(null);
 
   const lastScannedTime = useRef(0);
   const SCAN_DELAY = 1500;
-  const attendeesFileUri = FileSystem.documentDirectory + 'Attendence.csv';
   const flatListRef = useRef(null);
 
   useEffect(() => {
-    loadCSV();
+    async function fetchData() {
+      const data = await dataHandler.load();
+      setScannedList(data);
+    }
+    fetchData();
   },[])
 
-  // loads the initial csv file
-  const loadCSV = async () => {
-    // Check for file presence
-    const fileInfo = await FileSystem.getInfoAsync(attendeesFileUri);
-    console.log('File Exists? '+fileInfo.exists);
-    if(!fileInfo.exists){
-      await FileSystem.writeAsStringAsync(attendeesFileUri, '');
-      console.log('File created: '+attendeesFileUri)
-      const header = 'Time,ID'; // CSV header
-      await FileSystem.writeAsStringAsync(attendeesFileUri, header);
-      console.log('Attendees file created');
-    }
-    
-    // load data to array
-    const data = await FileSystem.readAsStringAsync(attendeesFileUri);
-    const rows = data.split('\n');
-    const parsedData = rows.map(row => {
-      const values = row.split(',');
-      return values;
-    });
-    // console.log(JSON.stringify(parsedData));
-    setScannedList(parsedData);
-  }
-
-  const updateCSV = async (newData) => {
-    let newRow = newData.join(',');
-    let fileData = await FileSystem.readAsStringAsync(attendeesFileUri, {encoding: FileSystem.EncodingType.UTF8});
-    const newFileData = fileData + '\n' + newRow;
-    await FileSystem.writeAsStringAsync(attendeesFileUri, newFileData);
-  }
-
-  // if no camera, exit
-  if(!permission){
-    return <View />;
-  }
-
-  // Ask for cam access
-  if(!permission.granted){
-    return (
-      <View style={styles.container}>
-        <Text style={{textAlign: 'center'}}>Please grant permission to access camera</Text>
-        <Button onPress={requestPermission} title="Grant Permission" />
-      </View>
-    )
-  }
-
-  // Append item to list
-  const listItem = async (qr) => {
+  // Handle QR scanning and processing
+  const handleQRScan = async (qr) => {
     // Delay QR Scan
     const now = Date.now();
     if(now - lastScannedTime.current < SCAN_DELAY) return;
@@ -100,6 +57,7 @@ export default function App() {
       }
     }
 
+    // Check if data already exists in list
     const index = scannedList.findIndex(e => e[1] === JSON.stringify(data));
     if(index>-1){
       ToastAndroid.show('Already Present', ToastAndroid.SHORT);
@@ -112,59 +70,73 @@ export default function App() {
     setOverlayColor("green");
     setTimeout(() => setOverlayColor(null), 500); // Reset after 500ms
 
-    //create new row
+    // Create new row for CSV
     console.log(lastScannedTime.current);
     const dt = new Date(lastScannedTime.current).toLocaleString().replace(', ',' ');
     console.log(dt);
     const newRow = [dt, JSON.stringify(data)];
 
-    //Update csvfile and list array 
-    updateCSV(newRow);
-    setScannedList((list) => {
-      return [...list, newRow];
-    });
+    // Update CSV file and list array 
+    await dataHandler.save(newRow);
+    setScannedList((list) => [...list, newRow]);
     setTimeout(()=>{  // this feels wrong, but is important to make sure re-render happens before scroll
       flatListRef.current?.scrollToEnd({ animated: true });
     },100);
-
-    //Toast🍻
     ToastAndroid.show(JSON.stringify(data).substring(0,10)+' added', ToastAndroid.SHORT);
   }
   
   return (
     <AppContext.Provider value={{isInfoVisible, setInfoVisible, secretKey, setSecretKey, attendeesFileUri}}>
       <View style={styles.container}>
-        {/* Camera view */}
-        <CameraView 
-          style={styles.camera}
-          barcodeScannerSettings={{
-            barcodeTypes: ["qr"],
-          }}
-          onBarcodeScanned={listItem}>
-            <View style={styles.camOverlay}/>
-        </CameraView>
-
         {overlayColor === "green" && <View style={styles.overlayGreen} />}
         {overlayColor === "red" && <View style={styles.overlayRed} />}
+        <CamPermissionAlert />        
+        <Camera handleQRScan={handleQRScan} />        
+        <ScannedList scannedList={scannedList} flatListRef={flatListRef} />
+        <InfoModal />
+        <InfoButton />
+        <ShareButton />
+      </View>
+    </AppContext.Provider>
+  );
+}
 
-        {/* List view */}
+function CamPermissionAlert(){
+  const [permission, requestPermission] = useCameraPermissions();
+  
+  useEffect(() => {
+    if(permission == null) return;
+    if(!permission.granted) {
+      requestPermission();
+    } else {
+      console.log("Camera permission granted");
+    }
+  },[permission]);
+}
 
-        <FlatList style={{flex: 1}}
+function Camera({handleQRScan}){
+  return (
+    <CameraView 
+    style={styles.camera}
+    barcodeScannerSettings={{
+      barcodeTypes: ["qr"],
+    }}
+    onBarcodeScanned={handleQRScan}>
+      <View style={styles.camOverlay}/>
+    </CameraView>
+  )
+}
+
+function ScannedList({scannedList, flatListRef}){
+  return (
+    <FlatList style={{flex: 1}}
         ref={flatListRef}
           data={scannedList}
           renderItem={(data) => <ScannedItem data={data}/>}
           keyExtractor={(data,idx) => idx}
           inverted
         />
-  
-        {/* Other not important things*/}
-        <InfoModal />
-        <InfoButton />
-        <ShareButton />
-      </View>
-
-    </AppContext.Provider>
-  );
+  )
 }
 
 function InfoModal(){
@@ -184,17 +156,6 @@ function InfoModal(){
         value={secretKey}
         placeholder="Enter Token here"
       />
-
-        <Text style={styles.text}>
-        Developer: 
-        <Text style={{color: 'blue'}}
-          onPress={() => Linking.openURL('http://www.github.com/Ashishjii')}>
-          Ash
-        </Text>
-      </Text>
-      <Button title='Go back'
-        onPress={()=> setInfoVisible(false)}
-        color='midnightblue' />
     </Modal>
   )
 }
