3ad72c8af9bbcd9898a89ab09890946245fb97e0
Parth Agarwal <agarwalparthsplendid@gmail.com>
2024-09-23 16:55:59 +0530

    bottom navigation

--- app/Event.js
+++ app/Event.js
@@ -1,94 +1,141 @@
-import { Text, View, FlatList } from "react-native";
-import { useLocalSearchParams, useRouter } from "expo-router";
-import SQLiteDbHandler from "../data/SQLiteDbHandler";
-// import styles from '../styles/indexStyles';
-import { PaperProvider, Button } from "react-native-paper"; // Import Button from react-native-paper
+import * as React from "react";
+import { BottomNavigation, Text } from "react-native-paper";
 
-const dbHandler = new SQLiteDbHandler();
+const RegistrationsRoute = () => <Text>Registrations</Text>;
 
-export default function Event() {
-  const { event } = useLocalSearchParams(); // Get the event parameter
-  const eventDetails = JSON.parse(event); // Parse the event details
-  const router = useRouter(); // Initialize the router
+const TicketsRoute = () => <Text>Tickets</Text>;
+
+const ScannerRoute = () => <Text>Scanner</Text>;
+
+const CertificatesRoute = () => <Text>Certificates</Text>;
+
+const Event = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {
+      key: "Registrations",
+      title: "Registrations",
+      focusedIcon: "format-list-bulleted-square",
+      unfocusedIcon: "format-list-checkbox",
+    },
+    { key: "Tickets", title: "Tickets", focusedIcon: "ticket-account", unfocusedIcon:"ticket-outline"},
+    { key: "Scanner", title: "Scanner", focusedIcon: "qrcode-scan", unfocusedIcon:"line-scan" },
+    {
+      key: "Certificates",
+      title: "Certificates",
+      focusedIcon: "certificate",
+      unfocusedIcon: "certificate-outline",
+    },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    Registrations: RegistrationsRoute,
+    Tickets: TicketsRoute,
+    Scanner: ScannerRoute,
+    Certificates: CertificatesRoute,
+  });
 
   return (
-    <PaperProvider>
-      <View contentContainerStyle={styles.container}>
-        <Text style={styles.title}>{eventDetails.name}</Text>
-        <Text>{eventDetails.date}</Text>
-
-        <View style={styles.linkContainer}>
-          {/* Replace Link with Paper's Button */}
-          <Button
-            mode="contained"
-            onPress={() =>
-              router.push({
-                pathname: "/Qr",
-                params: { event: JSON.stringify(eventDetails) },
-              })
-            }
-            style={styles.linkButton}
-            rippleColor="#ff000020"
-          >
-            Generate QR 📄
-          </Button>
-
-          <Button
-            mode="contained"
-            onPress={() =>
-              router.push({
-                pathname: "/Email",
-                params: { event: JSON.stringify(eventDetails) },
-              })
-            }
-            style={styles.linkButton}
-            rippleColor="#ff000020"
-          >
-            Send Mail 📨
-          </Button>
-
-          <Button
-            mode="contained"
-            onPress={() =>
-              router.push({
-                pathname: "/Scanner",
-                params: { event: JSON.stringify(eventDetails) },
-              })
-            }
-            style={styles.linkButton}
-            rippleColor="#ff000020"
-          >
-            Scan Entry 📷
-          </Button>
-        </View>
-
-        {/* Participants List */}
-        <Text style={styles.subtitle}>Participants:</Text>
-        <FlatList
-          data={eventDetails.participants} // Assuming participants is an array in eventDetails
-          keyExtractor={(item) => item.rollNumber} // Unique key for each participant
-          renderItem={({ item }) => (
-            <View style={styles.participantItem}>
-              <Text>
-                {item.name} - {item.rollNumber}
-              </Text>
-            </View>
-          )}
-        />
-
-        {/* Go Back Button */}
-        <Button
-          mode="outlined"
-          onPress={() => router.back()}
-          style={styles.backButton}
-          rippleColor="#ff000020"
-        >
-          Go Back
-        </Button>
-      </View>
-    </PaperProvider>
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
   );
-}
+};
+
+export default Event;
+// import { Text, View, FlatList } from "react-native";
+// import { useLocalSearchParams, useRouter } from "expo-router";
+// import SQLiteDbHandler from "../data/SQLiteDbHandler";
+// // import styles from '../styles/indexStyles';
+// import { PaperProvider, Button } from "react-native-paper"; // Import Button from react-native-paper
+
+// const dbHandler = new SQLiteDbHandler();
+
+// export default function Event() {
+//   const { event } = useLocalSearchParams(); // Get the event parameter
+//   const eventDetails = JSON.parse(event); // Parse the event details
+//   const router = useRouter(); // Initialize the router
+
+//   return (
+//     <PaperProvider>
+//       <View contentContainerStyle={styles.container}>
+//         <Text style={styles.title}>{eventDetails.name}</Text>
+//         <Text>{eventDetails.date}</Text>
+
+//         <View style={styles.linkContainer}>
+//           {/* Replace Link with Paper's Button */}
+//           <Button
+//             mode="contained"
+//             onPress={() =>
+//               router.push({
+//                 pathname: "/Qr",
+//                 params: { event: JSON.stringify(eventDetails) },
+//               })
+//             }
+//             style={styles.linkButton}
+//             rippleColor="#ff000020"
+//           >
+//             Generate QR 📄
+//           </Button>
+
+//           <Button
+//             mode="contained"
+//             onPress={() =>
+//               router.push({
+//                 pathname: "/Email",
+//                 params: { event: JSON.stringify(eventDetails) },
+//               })
+//             }
+//             style={styles.linkButton}
+//             rippleColor="#ff000020"
+//           >
+//             Send Mail 📨
+//           </Button>
+
+//           <Button
+//             mode="contained"
+//             onPress={() =>
+//               router.push({
+//                 pathname: "/Scanner",
+//                 params: { event: JSON.stringify(eventDetails) },
+//               })
+//             }
+//             style={styles.linkButton}
+//             rippleColor="#ff000020"
+//           >
+//             Scan Entry 📷
+//           </Button>
+//         </View>
+
+//         {/* Participants List */}
+//         <Text style={styles.subtitle}>Participants:</Text>
+//         <FlatList
+//           data={eventDetails.participants} // Assuming participants is an array in eventDetails
+//           keyExtractor={(item) => item.rollNumber} // Unique key for each participant
+//           renderItem={({ item }) => (
+//             <View style={styles.participantItem}>
+//               <Text>
+//                 {item.name} - {item.rollNumber}
+//               </Text>
+//             </View>
+//           )}
+//         />
+
+//         {/* Go Back Button */}
+//         <Button
+//           mode="outlined"
+//           onPress={() => router.back()}
+//           style={styles.backButton}
+//           rippleColor="#ff000020"
+//         >
+//           Go Back
+//         </Button>
+//       </View>
+//     </PaperProvider>
+//   );
+// }
 
 // function DebugMenu(){
 //     const [modalVisible, setModalVisible] = useState(false);
--- data/Events.js
+++ data/Events.js
@@ -2,39 +2,6 @@
 
 const events = [
     {
-        name: "Hustlers",
-        date: "30 September",
-        secretKey: "Hustlers",
-        participants: [
-            {
-                name: "participant 1",
-                rollNo: "01",
-                email: "abc@abc.in",
-            },
-            {
-                name: "participant 2",
-                rollNo: "02",
-                email: "abc@abc.in",
-            }
-        ],
-    },    {
-        name: "Hustlers",
-        date: "30 September",
-        secretKey: "Hustlers",
-        participants: [
-            {
-                name: "participant 1",
-                rollNo: "01",
-                email: "abc@abc.in",
-            },
-            {
-                name: "participant 2",
-                rollNo: "02",
-                email: "abc@abc.in",
-            }
-        ],
-    },
-    {
         name: "Startup Charcha",
         date: "01 September", 
         secretKey: "Startup Charcha",
